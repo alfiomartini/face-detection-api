617ac2f9dc1caff932a4e99430a8486091c087df
Alfio Martini <alfio.martini@gmail.com>
2020-12-14 18:33:53 -0300

    Updated db connection (10)

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ const { PG_DATABASE } = require('./config')
 
 const db = knex({
   client: 'pg',
+  // connection to local postgres
+  // connection: PG_DATABASE
+  // connection to heroku
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: {
