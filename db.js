f65d8a3d4f39f33c0283a094f36d30b10b66adba
Alfio Martini <alfio.martini@gmail.com>
2020-12-14 15:56:00 -0300

    Updated db connection

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const  knex = require('knex');
 
 const { DATABASE_URL } = require('./config')
+
 const db = knex({
   client: 'pg',
-  connection:DATABASE_URL
+  connection:'postgres://sjuchpxsdglslv:fc38b8373747132da269cc38d1f8e32de3ecbd88259c04f6616aaca155718bfe@ec2-52-6-75-198.compute-1.amazonaws.com:5432/damv9mq4qmj66i'
 });
 
 module.exports = { db };
