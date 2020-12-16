8b98242c26c45ec817630469ad5a9fc62e941440
Alfio Martini <alfio.martini@gmail.com>
2020-12-16 15:33:14 -0300

    Updated config.js (comments)

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+// https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
+
+// https://www.twilio.com/blog/working-with-environment-variables-in-node-js-html
+
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
